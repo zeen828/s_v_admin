fa847ed18e372c813ef1aabb598206de1ac79198
Will Lu <zeren828@gmail.com>
2017-05-22 12:10:37 +0800

    fff

--- application/controllers/cron/Votes.php
+++ application/controllers/cron/Votes.php
@@ -34,6 +34,7 @@ class Votes extends CI_Controller
 			$this->load->model ( 'vidol_websocket/mrplay_model' );
 			echo 'HI';
 			$cron = $this->mrplay_model->get_row_Mrplay_cronno('cron_no');
+			print_r($cron);
 			if(empty($cron)){
 				$cron_no = 0;
 			}else{
