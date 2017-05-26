4e6d1e783b45256b23f4f20c81fa3fc43fb3b3dc
Will Lu <zeren828@gmail.com>
2017-05-26 20:20:16 +0800

    dev

--- application/models/postgre/Vidol_production_model.php
+++ application/models/postgre/Vidol_production_model.php
@@ -268,7 +268,7 @@ class vidol_production_model extends CI_Model {
 		$this->r_db->group_by ( 'member_id' );
 		$this->r_db->from ( 'mrplayer_votes' );
 		$count = $this->r_db->count_all_results ();
-		// echo $this->r_db->last_query();
+		echo $this->r_db->last_query();
 		return $count;
 	}
 }
