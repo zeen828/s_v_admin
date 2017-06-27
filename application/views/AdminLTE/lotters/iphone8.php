83bae0c18dfb1172dde4214d7cf0e5b9d0cf1856
Will Lu <zeren828@gmail.com>
2017-06-27 18:00:39 +0800

    dev

--- application/views/AdminLTE/lotters/iphone8.php
+++ application/views/AdminLTE/lotters/iphone8.php
@@ -1,26 +1,6 @@
-<<script type="text/javascript">
+<script type="text/javascript">
 $('#lotters_clear').off('click').on('click', function() {
 	alert('清空');
-	$.ajax({
-		url: '/api/lotters/clear.json',
-		type: 'GET',
-		cache: false,
-		headers: {
-			'Authorization' : 'sw84sc888kkcg0ogo8cw4swgkswkw048cc48swk8'
-		},
-		dataType: 'json',
-		data: {
-			'debug' : 'debug'
-		},
-		error: function(xhr){
-			alert('Ajax request error');
-		},
-		statusCode: {
-			200: function(json, statusText, xhr) {
-				console.log(json);
-			}
-		}
-	});
 });
 </script>
 					<div><button type="button" id="lotters_clear">清空得獎清單</button></div>
