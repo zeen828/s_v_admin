3b469d76ae2ea4c29064b446bb14cda97a1ee104
Will Lu <zeren828@gmail.com>
2017-07-04 14:27:00 +0800

    dev

--- application/controllers/backend/Lotteries.php
+++ application/controllers/backend/Lotteries.php
@@ -55,6 +55,73 @@ class Lotteries extends CI_Controller {
 		}
 	}
 
+	public function openlist($pk) {
+		try {
+			if ($this->flexi_auth->is_privileged('Lottery Config View')) {
+				// 寫log
+				$this->fun->logs('觀看抽獎系統設定結果');
+				// 變數
+				$data_post = array();
+				// 強制切換資料庫
+				unset($this->db);
+				$this->db = $this->load->database('vidol_old_write', true);
+				// grocery_CRUD 自產表單
+				$this->load->library('grocery_CRUD'); // CI整合表單http://www.grocerycrud.com/
+				$this->load->library('vidol/grocery_callback');
+				$crud = new grocery_CRUD();
+				// 語系
+				$crud->set_language('taiwan');
+				// 版型
+				$crud->set_theme('flexigrid');
+				// 表格
+				$crud->set_table('lotters_config_tbl');
+				// 標題
+				$crud->set_subject('抽獎系統設定檔');
+				// 移除新增
+				//$crud->unset_add();
+				// 移除編輯
+				//$crud->unset_edit();
+				// 移除刪除
+				//$crud->unset_delete();
+				// 清單顯示欄位
+				$crud->columns('lc_pk', 'lc_title', 'lc_des', 'lc_method', 'lc_status', 'lc_start_at', 'lc_end_at');
+				// 事件
+				$crud->add_action('開獎頁', '/assets/grocery_crud/themes/flexigrid/css/images/export.png', '', '', array($this->grocery_callback,'callback_config_to_lotters_url'));
+				$crud->add_action('開獎名單', '/assets/grocery_crud/themes/flexigrid/css/images/export.png', 'openlist');
+				// 資料庫欄位文字替換
+				$crud->display_as('lc_pk', $this->lang->line('fields_pk'));
+				$crud->display_as('lc_title', '標題');
+				$crud->display_as('lc_des', '描述');
+				$crud->display_as('lc_db_type', '資料庫類型');
+				$crud->display_as('lc_db_table', '資料庫表');
+				$crud->display_as('lc_db_where', '條件');
+				$crud->display_as('lc_value1', '數值1');
+				$crud->display_as('lc_value2', '數值2');
+				$crud->display_as('lc_value3', '數值3');
+				$crud->display_as('lc_value4', '數值4');
+				$crud->display_as('lc_value5', '數值5');
+				$crud->display_as('lc_method', '開講方法');
+				$crud->display_as('lc_status', '狀態');
+				$crud->display_as('lc_start_at', '開始時間');
+				$crud->display_as('lc_end_at', '結束時間');
+				// 產生表單
+				$output = $crud->render();
+				// 資料整理
+				$this->data_view['right_countent']['view_path'] = 'AdminLTE/include/content_grocery_crud';
+				$this->data_view['right_countent']['view_data'] = $output;
+				$this->data_view['right_countent']['tags']['tag_3'] = array(
+						'title' => '抽獎系統',
+						'link' => '/backend/lotteries/system',
+						'class' => 'fa-cog'
+				);
+				// 套版
+				$this->load->view('AdminLTE/include/html5', $this->data_view);
+			}
+		} catch ( Exception $e ) {
+			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
+		}
+	}
+
 	public function system() {
 		try {
 			if ($this->flexi_auth->is_privileged('Lottery Config View')) {
