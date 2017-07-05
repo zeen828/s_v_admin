19454d88d468b43c48d14a629af4b31d2dc2580d
Will Lu <zeren828@gmail.com>
2017-07-05 17:27:56 +0800

    dev

--- application/models/vidol_websocket/Boards_model.php
+++ application/models/vidol_websocket/Boards_model.php
@@ -79,6 +79,7 @@ class Boards_model extends CI_Model {
 		if (! empty ( $where_string )) {
 			$this->r_db->where ( $where_string );
 		}
+		$this->r_db->order_by($count, 'RANDOM');
 		$query = $this->r_db->get ( 'Board_tbl' );
 		echo $this->r_db->last_query ();
 		return $query;
