aab078151c8d461fbb8b5e290166f0abb1008ca3
Will Lu <zeren828@gmail.com>
2017-08-16 09:07:41 +0800

    dev

--- application/models/vidol_event/Event_vote_select_model.php
+++ application/models/vidol_event/Event_vote_select_model.php
@@ -67,12 +67,12 @@ class Event_vote_select_model extends CI_Model {
 	}
 	//不重複投票數
 	public function get_single_vote_count_by_configid_date($config_id, $start_date, $end_date) {
-		$this->r_db->select ( '*, count(id) as count' );
+		$this->r_db->select ( '*, count(id) as count_no' );
 		$this->r_db->where ( 'config_id', $config_id );
 		$this->r_db->group_by ( 'user_id' );
 		$this->r_db->order_by('created_at', 'ASC');
 		$sql = $this->r_db->get_compiled_select ( 'event_vote_select_tbl' );
-		$sql = sprintf("SELECT count(*) FROM ( %s ) WHERE count = 1 AND created_at >= '%s' AND created_at < '%s' ", $sql, $start_date, $end_date);
+		$sql = sprintf("SELECT count(*) FROM ( %s ) as t WHERE count_no = 1 AND created_at >= '%s' AND created_at < '%s' ", $sql, $start_date, $end_date);
 		echo $sql;
 		$count = 0;
 		return $count;
