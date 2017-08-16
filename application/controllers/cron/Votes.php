def54f1e37a21374cf02464e63bd570b75749936
Will Lu <zeren828@gmail.com>
2017-08-16 10:00:17 +0800

    dev

--- application/controllers/cron/Votes.php
+++ application/controllers/cron/Votes.php
@@ -541,9 +541,9 @@ class Votes extends CI_Controller {
 			$data_insert['vote'] = $this->event_vote_select_model->get_vote_count_by_configid_date($config_id, $date_arr['start'], $date_arr['end']);
 			//新投票會員
 			$data_insert['new_vote'] = $this->event_vote_select_model->get_new_vote_count_by_configid_date($config_id, $date_arr['start'], $date_arr['end']);
-			//不重複投票數
+			//不重複投票數(全部GROUP BY後再查昨天零晨到今天零晨只有一筆的)
 			$data_insert['single_vote'] = $this->event_vote_select_model->get_single_vote_count_by_configid_date($config_id, $date_arr['start'], $date_arr['end']);
-			//累計投票數
+			//累計投票數(到今天零晨)
 			$data_insert['total_vote'] = $this->event_vote_select_model->get_total_vote_count_by_configid_date($config_id, $date_arr['start'], $date_arr['end']);
 			//投票註冊數(昨天零晨到今天零晨)
 			$data_insert['registered'] = $this->event_vote_select_model->get_registered_count_by_configid_date($config_id, $date_arr['start'], $date_arr['end']);
