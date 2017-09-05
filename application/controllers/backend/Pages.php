936c90f5ac63b6eac41f10a69285a1d429e055f7
Will Lu <zeren828@gmail.com>
2017-09-05 15:56:40 +0800

    dev

--- application/controllers/backend/Pages.php
+++ application/controllers/backend/Pages.php
@@ -1,149 +1,144 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-class Pages extends CI_Controller
-{
-
+defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+class Pages extends CI_Controller {
 	private $data_view;
-
-	function __construct ()
-	{
-		parent::__construct();
+	function __construct() {
+		parent::__construct ();
 		// 資料庫
-		$this->load->database();
+		$this->load->database ();
 		// 權限
-		$this->auth = new stdClass();
-		$this->load->library('flexi_auth');
-		if (! $this->flexi_auth->is_logged_in()) {
-			redirect('homes/login');
+		$this->auth = new stdClass ();
+		$this->load->library ( 'flexi_auth' );
+		if (! $this->flexi_auth->is_logged_in ()) {
+			redirect ( 'homes/login' );
 		}
 		// 引用
-		$this->load->library('session');
-		$this->load->library('lib_log');
-		$this->load->library('vidol/fun');
-		$this->load->helper('formats');
-		$this->lang->load('table_vidol', 'traditional-chinese');
+		$this->load->library ( 'session' );
+		$this->load->library ( 'lib_log' );
+		$this->load->library ( 'vidol/fun' );
+		$this->load->helper ( 'formats' );
+		$this->lang->load ( 'table_vidol', 'traditional-chinese' );
 		// 初始化
-		$this->data_view = format_helper_backend_view_data('orders_content');
-		$this->data_view['system']['action'] = 'Pages';
-		$this->data_view['right_countent']['tags']['tag_2'] = array(
+		$this->data_view = format_helper_backend_view_data ( 'orders_content' );
+		$this->data_view ['system'] ['action'] = 'Pages';
+		$this->data_view ['right_countent'] ['tags'] ['tag_2'] = array (
 				'title' => '活動頁面管理',
 				'link' => '/backend/pages',
-				'class' => 'fa-money'
+				'class' => 'fa-money' 
 		);
 		// 效能檢查
 		// $this->output->enable_profiler(TRUE);
 	}
-
-	public function index ()
-	{
-		show_404();
+	public function index() {
+		show_404 ();
 	}
-	public function load_page ()
-	{
-		//$this->output->enable_profiler(TRUE);
+	public function load_page() {
+		// $this->output->enable_profiler(TRUE);
 		try {
-			if ($this->flexi_auth->is_privileged('Orders View')) {
+			if ($this->flexi_auth->is_privileged ( 'Orders View' )) {
 				// 寫log
-				$this->fun->logs('觀看中繼頁');
+				$this->fun->logs ( '觀看中繼頁' );
 				// 變數
-				$data_post = array();
+				$data_post = array ();
 				// 資料整理
-				$this->data_view['right_countent']['view_path'] = 'AdminLTE/pages/load_page';
-				//$this->data_view['right_countent']['view_data'] = $output;
-				$this->data_view['right_countent']['tags']['tag_3'] = array(
+				$this->data_view ['right_countent'] ['view_path'] = 'AdminLTE/pages/load_page';
+				// $this->data_view['right_countent']['view_data'] = $output;
+				$this->data_view ['right_countent'] ['tags'] ['tag_3'] = array (
 						'title' => '中繼頁',
 						'link' => '/backend/pages/load_page',
-						'class' => 'fa-building-o'
+						'class' => 'fa-building-o' 
 				);
 				// 套版
-				$this->load->view('AdminLTE/include/html5', $this->data_view);
+				$this->load->view ( 'AdminLTE/include/html5', $this->data_view );
 			}
-		} catch (Exception $e) {
-			show_error($e->getMessage() . ' --- ' . $e->getTraceAsString());
+		} catch ( Exception $e ) {
+			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
 		}
 	}
-	
-	public function load_page_channel ()
-	{
-		//$this->output->enable_profiler(TRUE);
+	public function load_page_channel() {
+		// $this->output->enable_profiler(TRUE);
 		try {
-			if ($this->flexi_auth->is_privileged('Orders View')) {
+			if ($this->flexi_auth->is_privileged ( 'Orders View' )) {
 				// 寫log
-				$this->fun->logs('觀看中繼頁');
+				$this->fun->logs ( '觀看中繼頁' );
 				// 變數
-				$data_post = array();
-				
-				$data_post = $this->input->post();
-				print_r($data_post);
-				print_r($_POST);
-				// 資料整理
-				$this->data_view['right_countent']['view_path'] = 'AdminLTE/pages/load_page';
-				//$this->data_view['right_countent']['view_data'] = $output;
-				$this->data_view['right_countent']['tags']['tag_3'] = array(
-						'title' => '中繼頁',
-						'link' => '/backend/pages/load_page',
-						'class' => 'fa-building-o'
-				);
-				// 套版
-				$this->load->view('AdminLTE/include/html5', $this->data_view);
+				$data_input = array ();
+				// 接收變數
+				$data_input ['channel_pk_1'] = $this->input->post ( 'channel_pk_1' );
+				$data_input ['channel_type_1'] = $this->input->post ( 'channel_type_1' );
+				$data_input ['channel_id_1'] = $this->input->post ( 'channel_id_1' );
+				$data_input ['channel_pk_2'] = $this->input->post ( 'channel_pk_2' );
+				$data_input ['channel_type_2'] = $this->input->post ( 'channel_type_2' );
+				$data_input ['channel_id_2'] = $this->input->post ( 'channel_id_2' );
+				// CALL API
+				$api_url = 'http://api-background.vidol.tv/v1/channels/';
+				$ch = curl_init ();
+				curl_setopt ( $ch, CURLOPT_URL, $api_url );
+				curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+				curl_setopt ( $ch, CURLOPT_HTTPHEADER, array (
+						'Content-Type: application/json',
+						'Accept: application/json',
+						'Authorization: Basic MWIyNjZkNjI0OWJiYjljM2M2ZDdkYjM0YWU1YzU5YzZhMzYyZmQxODgxOGJkMzM2NmNiYjY5YTUzOGYwZmU2NDpjODNlMDkxMmQ5MWI1NjAzM2RlNmFmODdjZDIxZGZkODk1NTBkNzA4M2Q3ODM0ZDIyMWVmNmNkZGM5ODg4ZjM2',
+						'locale: zh-tw' 
+				) );
+				$output = curl_exec ( $ch );
+				curl_close ( $ch );
+				// 改資料
+				$output = json_decode ( $output );
+				print_r ($output);
 			}
-		} catch (Exception $e) {
-			show_error($e->getMessage() . ' --- ' . $e->getTraceAsString());
+		} catch ( Exception $e ) {
+			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
 		}
 	}
-	
-	public function load_page_video ()
-	{
-		//$this->output->enable_profiler(TRUE);
+	public function load_page_video() {
+		// $this->output->enable_profiler(TRUE);
 		try {
-			if ($this->flexi_auth->is_privileged('Orders View')) {
+			if ($this->flexi_auth->is_privileged ( 'Orders View' )) {
 				// 寫log
-				$this->fun->logs('觀看中繼頁');
+				$this->fun->logs ( '觀看中繼頁' );
 				// 變數
-				$data_post = array();
+				$data_post = array ();
 				
-				print_r($_POST);
+				print_r ( $_POST );
 				// 資料整理
-				$this->data_view['right_countent']['view_path'] = 'AdminLTE/pages/load_page';
-				//$this->data_view['right_countent']['view_data'] = $output;
-				$this->data_view['right_countent']['tags']['tag_3'] = array(
+				$this->data_view ['right_countent'] ['view_path'] = 'AdminLTE/pages/load_page';
+				// $this->data_view['right_countent']['view_data'] = $output;
+				$this->data_view ['right_countent'] ['tags'] ['tag_3'] = array (
 						'title' => '中繼頁',
 						'link' => '/backend/pages/load_page',
-						'class' => 'fa-building-o'
+						'class' => 'fa-building-o' 
 				);
 				// 套版
-				$this->load->view('AdminLTE/include/html5', $this->data_view);
+				$this->load->view ( 'AdminLTE/include/html5', $this->data_view );
 			}
-		} catch (Exception $e) {
-			show_error($e->getMessage() . ' --- ' . $e->getTraceAsString());
+		} catch ( Exception $e ) {
+			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
 		}
 	}
-
-	public function load_page_event ()
-	{
-		//$this->output->enable_profiler(TRUE);
+	public function load_page_event() {
+		// $this->output->enable_profiler(TRUE);
 		try {
-			if ($this->flexi_auth->is_privileged('Orders View')) {
+			if ($this->flexi_auth->is_privileged ( 'Orders View' )) {
 				// 寫log
-				$this->fun->logs('觀看中繼頁');
+				$this->fun->logs ( '觀看中繼頁' );
 				// 變數
-				$data_post = array();
+				$data_post = array ();
 				
-				print_r($_POST);
+				print_r ( $_POST );
 				// 資料整理
-				$this->data_view['right_countent']['view_path'] = 'AdminLTE/pages/load_page';
-				//$this->data_view['right_countent']['view_data'] = $output;
-				$this->data_view['right_countent']['tags']['tag_3'] = array(
+				$this->data_view ['right_countent'] ['view_path'] = 'AdminLTE/pages/load_page';
+				// $this->data_view['right_countent']['view_data'] = $output;
+				$this->data_view ['right_countent'] ['tags'] ['tag_3'] = array (
 						'title' => '中繼頁',
 						'link' => '/backend/pages/load_page',
-						'class' => 'fa-building-o'
+						'class' => 'fa-building-o' 
 				);
 				// 套版
-				$this->load->view('AdminLTE/include/html5', $this->data_view);
+				$this->load->view ( 'AdminLTE/include/html5', $this->data_view );
 			}
-		} catch (Exception $e) {
-			show_error($e->getMessage() . ' --- ' . $e->getTraceAsString());
+		} catch ( Exception $e ) {
+			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
 		}
 	}
 }
