decca08d604c6aa37d2039f147813449eeb3d3c5
Will Lu <zeren828@gmail.com>
2017-09-21 16:13:14 +0800

    dev

--- assets/js/custom/events/lottery/lottery.js
+++ assets/js/custom/events/lottery/lottery.js
@@ -7,56 +7,15 @@ var running = false;
 var start_date = '';
 var end_date = ''
 function getLottery(start_date, end_date){
-	$.ajax({
-		url: '/api/lottery/iphone8',
-		type: 'GET',
-		cache: false,
-		dataType: 'json',
-		data: {
-			'token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZGVudGl0eSI6eyJpZCI6NDMyMDc0LCJ1aWQiOiJzM2dYR0JDbUYwIiwiZW1haWxfdmVyaWZpZWQiOnRydWV9LCJhcHBsaWNhdGlvbl9pZCI6MSwiZXhwaXJlc19hdCI6MTQ4NDcyODY3NiwicmFuZF9rZXkiOiI5NDA1ZGMwYmQxMGE2ZGMwMTA1NGFiZGI1ZjQ2NzVhMiJ9.0gF3EgZhHHnxoZdWDqM4UBlCEKR9EPaW0qSFZrRsRuBlfgxhEqb_qR2vQzGdoLeC8bdAaIl1MC_2s7xE8wjMxQ',
-			'start' : start_date,
-			'end' : end_date
-		},
-		error: function(xhr){
-			console.log('Ajax request error');
-		},
-		success: function(response) {
-			console.log('Ajax OK');
-		},
-		statusCode: {
-			200: function(json, statusText, xhr) {
-				g_Lottery = json.lottery;
-			}
-		}
-	});
+	console.log('getLottery');
 }
 
 function writeLotteryList(){
-	$.ajax({
-		url: '/api/lottery/iphone8',
-		type: 'POST',
-		cache: false,
-		dataType: 'json',
-		data: {
-			'token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpZGVudGl0eSI6eyJpZCI6NDMyMDc0LCJ1aWQiOiJzM2dYR0JDbUYwIiwiZW1haWxfdmVyaWZpZWQiOnRydWV9LCJhcHBsaWNhdGlvbl9pZCI6MSwiZXhwaXJlc19hdCI6MTQ4NDcyODY3NiwicmFuZF9rZXkiOiI5NDA1ZGMwYmQxMGE2ZGMwMTA1NGFiZGI1ZjQ2NzVhMiJ9.0gF3EgZhHHnxoZdWDqM4UBlCEKR9EPaW0qSFZrRsRuBlfgxhEqb_qR2vQzGdoLeC8bdAaIl1MC_2s7xE8wjMxQ',
-			'mongo_id' : $('#ResultNum').html(),
-			'member_id' : $('#ResultNum').html()
-		},
-		error: function(xhr){
-			console.log('Ajax request error');
-		},
-		success: function(response) {
-			console.log('Ajax OK');
-		},
-		statusCode: {
-			200: function(json, statusText, xhr) {
-				console.log('Ajax OK 200');
-			}
-		}
-	});
+	console.log('writeLotteryList');
 }
 
 function getRandomArrayElements(arr, count) {
+	console.log('getRandomArrayElements');
     var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
     while (i-- > min) {
         index = Math.floor((i + 1) * Math.random());
@@ -68,6 +27,7 @@ function getRandomArrayElements(arr, count) {
 }
 
 function beginRndNum(trigger){
+	console.log('beginRndNum');
 	if(running){
 		if(g_Lottery.length >= 1){
 			var user = getRandomArrayElements(g_Lottery, 1);
@@ -95,15 +55,18 @@ function beginRndNum(trigger){
 }
 
 function updateRndNum(){
+	console.log('updateRndNum');
 	var user = getRandomArrayElements(g_LotteryList, 1);
 	$('#ResultNum').html(user[0].member_id);
 }
 
 function beginTimer(){
+	console.log('beginTimer');
 	g_Timer = setTimeout(beat, g_Interval);
 }
 
 function beat() {
+	console.log('beat');
 	g_Timer = setTimeout(beat, g_Interval);
 	updateRndNum();
 }
