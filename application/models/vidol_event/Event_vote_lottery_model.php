b8eab378e00fcfe5d1b3dd6a9a7ad29e0717b0ef
Will Lu <zeren828@gmail.com>
2017-09-22 16:31:36 +0800

    dev

--- application/models/vidol_event/Event_vote_lottery_model.php
+++ application/models/vidol_event/Event_vote_lottery_model.php
@@ -52,8 +52,8 @@ class Event_vote_lottery_model extends CI_Model {
 	// 檢查重複
 	public function confirm_repeat($config_id, $mongo_id, $member_id) {
 		$this->r_db->select('*')->from($this->table_name)
-		->group_start()
 		->where('a', 'a')
+		->group_start()
 		->where('b', 'b')
 		->where('c', 'c')
 		->group_end()
