3ff5529a265b29987dcfdf52bb512d7dafd72b2a
Will Lu <zeren828@gmail.com>
2017-09-28 11:22:58 +0800

    dev

--- application/views/AdminLTE/pages/landing_page.php
+++ application/views/AdminLTE/pages/landing_page.php
@@ -61,7 +61,7 @@ if(count($view_data['channel'])>=1){
 					<div class="box box-default">
 						<form action="/backend/pages/landing_page_text" method="post">
 						<div class="box-header with-border">
-							<h3 class="box-title">頻道</h3>
+							<h3 class="box-title">跑馬燈</h3>
 							<div class="box-tools pull-right">
 								<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
 								<button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
