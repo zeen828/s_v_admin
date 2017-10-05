e8bb026bbb334a953e5df1a1c475561158b5ce21
Will Lu <zeren828@gmail.com>
2017-10-05 10:41:37 +0800

    dev

--- application/controllers/backend/Vote_config.php
+++ application/controllers/backend/Vote_config.php
@@ -302,9 +302,9 @@ class Vote_config extends CI_Controller {
 				// 移除刪除
 				$crud->unset_delete ();
 				// 清單顯示欄位
-				$crud->columns ( 'item_id', 'member_id' );
+				$crud->columns ( 'config_id', 'item_id', 'member_id' );
 				// 關聯
-				//$crud->set_relation('item_id', 'event_vote_config_tbl', '{title}', array(), 'id');
+				$crud->set_relation('config_id', 'event_vote_config_tbl', '{title}', array(), 'id');
 				//$crud->set_relation('item_id', 'event_vote_item_tbl', '{group_title}{title}', array('config'=>'2'), 'sort');
 				//$crud->set_relation('item_id', 'vidol.user_groups', '{ugrp_name}', array(), 'ugrp_id');
 				// 資料庫欄位文字替換
